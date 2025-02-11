52569900cf10ed23448c6d8610ff58b96cefc421
Mikhail Olianenko <mikhail.olianenko@gmail.com>
2025-02-11 23:09:21 +1100

    добавил Вакансии в menuMobile

--- constants/practicalPaymentForm.ts
+++ constants/practicalPaymentForm.ts
@@ -1,11 +1,11 @@
 const formList = [
-  {label: '1-ая ступень', value: 'one'},
-  {label: '1-ая ступень + программа проф.переподготовки*', value: 'onePlusProf'},
-  {label: '1+2+3 ступени (-15 000)', value: 'all'},
-  {label: '2-ая ступень', value: 'two'},
-  {label: '2-ая ступень + программа проф.переподготовки*', value: 'twoPlusProf'},
-  {label: '3-я ступень', value: 'three'},
-  {label: '3-я ступень + программа проф.переподготовки*', value: 'threePlusProf'}
+  { label: '1-ая ступень', value: 'one' },
+  { label: '1-ая ступень + программа проф.переподготовки*', value: 'onePlusProf' },
+  { label: '1+2+3 ступени (-15 000)*', value: 'all' },
+  { label: '2-ая ступень', value: 'two' },
+  { label: '2-ая ступень + программа проф.переподготовки*', value: 'twoPlusProf' },
+  { label: '3-я ступень', value: 'three' },
+  { label: '3-я ступень + программа проф.переподготовки*', value: 'threePlusProf' }
 ]
 
 export default formList
