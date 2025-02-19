a74c260f7711b30418b6ab03e399fc1db2380c2b
Ivan Kolpakov <vanjaklp95@gmail.com>
2025-02-19 19:42:21 +0300

    connected isr to partners

---
+++ app/partners/[type]/page.tsx
@@ -0,0 +1,87 @@
+import routes from '@/config/routes'
+import axios from 'axios'
+import React from 'react'
+import qs from 'qs'
+
+
+interface partners {
+  data: {
+    data: {
+      id: string
+
+    }[]
+  }
+  title: string
+  content: string
+}
+
+const queryString = qs.stringify(
+  {
+
+    populate: '*'
+  },
+  {
+    encodeValuesOnly: true,
+    skipNulls: true
+  }
+)
+
+export const revalidate = 60
+export const dynamicParams = true 
+
+export async function generateStaticParams() {
+  const partners =  await axios.get(`${routes.back.rootv2}/api/partners?${queryString}`, {
+    headers: {
+      Authorization: `Bearer ${process.env.STRAPI_BEARER}` // Замените на ваш токен
+    }
+  })
+
+  const categories = Array.from(
+    new Set(partners?.data?.data?.map((partner) => partner.type))
+  )
+  console.log('AAA',categories);
+  
+  return categories.map((type) => ({ type }))
+}
+
+export default async function Partners({
+  params,
+}: {
+  params: Promise<{ type: string }>
+}) {
+
+  
+  const type = (await params).type
+
+  const filter = qs.stringify(
+    {
+      filters: {
+        type: {
+          $eq: type
+        }
+      },
+      populate: '*'
+    },
+    {
+      encodeValuesOnly: true,
+      skipNulls: true
+    }
+  )
+
+  const onePartner =  await axios.get(`${routes.back.rootv2}/api/partners?${filter}`, {
+    headers: {
+      Authorization: `Bearer ${process.env.STRAPI_BEARER}` // Замените на ваш токен
+    }
+  }).then((partner) => partner.data.data)
+
+  console.log('typeDD',onePartner);
+  
+  return (
+    <div>
+      <h1>lalalss</h1>
+      <p>{onePartner.map(partner => (
+        <p key={partner.id}>{partner.title}</p>
+      ))}</p>
+    </div>
+  )
+}
