65943474a3c303d365c2bc9d0e4b891bdae01293
Ivan Kolpakov <vanjaklp95@gmail.com>
2025-02-25 11:36:04 +0300

    revert without partners

--- pages/incomers.tsx
+++ pages/incomers.tsx
@@ -4,9 +4,10 @@ import { GetStaticProps } from 'next'
 import { NextSeo } from 'next-seo'
 
 const IncomersIndexPage = ({ incomers }) => {
+  
   return (
     <>
-    
+
       <NextSeo title='Поступление в МИП (Московский Институт Психологии)' description='Подробная информация для поступающих и абитуриентов Московского Института Психологии (МИП): условия поступления, широкий выбор программ, учебный процесс, условия оплаты, а так же помощь в подборе программы от наших специалистов.' noindex={true} nofollow={true} />
       <PageIncomers incomers={incomers} />
     </>
