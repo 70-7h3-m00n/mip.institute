51370afe650a810f7311ef52d96d93daf3bfe8b9
dv.udanov <dv.udanov@tensor.ru>
2025-02-27 13:01:22 +0300

    Создал скелет нижнего блока. Поправил стили у верхнего блока.

--- app/partners/[type]/loading.tsx
+++ app/partners/[type]/loading.tsx
@@ -1,12 +1,14 @@
 'use client'
 import OurPartnersSkelet from '@/components/partners/OurPartners/OurPartnersSkelet'
 import CommunitySectionSkelet from '@/components/partners/CommunitySection/CommunitySectionSkelet'
+import BecomePartnerSkeleton from '@/components/partners/BecomePartner/BecomePartnerSkeleton'
 
 export default function Loading() {
   return (
     <>
       <CommunitySectionSkelet />
       <OurPartnersSkelet />
+      <BecomePartnerSkeleton/>
     </>
   )
 }
