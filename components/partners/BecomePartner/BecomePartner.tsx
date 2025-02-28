09111350096f749b4e756ea3be5037dd1c86612f
dv.udanov <dv.udanov@tensor.ru>
2025-02-28 14:30:37 +0300

    Поменял заголовок

--- components/partners/BecomePartner/BecomePartner.tsx
+++ components/partners/BecomePartner/BecomePartner.tsx
@@ -12,11 +12,7 @@ export default function BecomePartner() {
       <Wrapper>
         <div className={styles.content}>
           <div className={styles.left}>
-            <h2 className={styles.title}>
-              {isMobileAndTabletLayout
-                ? 'Подберем программу под ваш запрос'
-                : 'Стать партнером – просто!'}
-            </h2>
+            <h2 className={styles.title}>Стать партнером – просто!</h2>
             <p className={styles.description}>
               Заполните короткую форму, и мы свяжемся с вами,
               {!isMobileAndTabletLayout && <br />} чтобы обсудить все детали
