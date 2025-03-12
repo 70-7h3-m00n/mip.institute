781233748f77b47768f688878de0d78cf3303373
Mikhail Olianenko <mikhail.olianenko@gmail.com>
2025-03-12 17:17:39 +1100

    исправил каноникал incomers

--- pages/incomers.tsx
+++ pages/incomers.tsx
@@ -3,13 +3,18 @@ import { getStaticPropsIncomers } from '@/lib/handlers/getStaticPropsIncomers'
 import { GetStaticProps } from 'next'
 import { NextSeo } from 'next-seo'
 import { preview } from '../config'
+import routes from '@/config/routes'
 
 const IncomersIndexPage = ({ incomers }) => {
-  
   return (
     <>
-
-      <NextSeo title='Поступление в МИП (Московский Институт Психологии)' description='Подробная информация для поступающих и абитуриентов Московского Института Психологии (МИП): условия поступления, широкий выбор программ, учебный процесс, условия оплаты, а так же помощь в подборе программы от наших специалистов.' noindex={preview} nofollow={preview} />
+      <NextSeo
+        title='Поступление в МИП (Московский Институт Психологии)'
+        description='Подробная информация для поступающих и абитуриентов Московского Института Психологии (МИП): условия поступления, широкий выбор программ, учебный процесс, условия оплаты, а так же помощь в подборе программы от наших специалистов.'
+        noindex={preview}
+        nofollow={preview}
+        canonical={`${routes.front.root}${routes.front.incomers}`}
+      />
       <PageIncomers incomers={incomers} />
     </>
   )
