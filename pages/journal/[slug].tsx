3866bfeb93dcc5e2736143f5dde838d348d7bcff
Ivan Kolpakov <vanjaklp95@gmail.com>
2025-03-17 17:13:18 +0300

    remove testing p tags

--- pages/journal/[slug].tsx
+++ pages/journal/[slug].tsx
@@ -13,9 +13,6 @@ import stls from '@/styles/pages/JournalSlug.module.sass'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { getStaticPropsBlog } from '@/lib/handlers/getStaticPropsBlog'
 import { getStaticPathsBlogs } from '@/lib/getStaticPaths/getStaticPathsBlog'
-import routes from '@/config/routes'
-import preview from '@/config/preview'
-import prod from '@/config/prod'
 
 const JournalSlugPage = ({ blog }) => {
   console.log(blog);
@@ -51,17 +48,10 @@ const JournalSlugPage = ({ blog }) => {
     return breadcrumb
   })
 
-  console.log(preview);
-  
-
   return (
     <Wrapper>
       <SeoPagesJournal blog={blog} />
       <div className={stls.in}>
-      <p>root2 {routes.back.rootv2 }</p>
-      <p>root1 {routes.back.root}</p>
-      <p>preview {String(preview)}</p>
-      <p>prod {String(prod)}</p>
         <ReadingProgressbar />
         <Breadcrumbs isJournal journalSlug={blog?.studyFieldSlug} lastLabel={blog?.studyField}/>
         {articleHeading && <ArticleTitle props={articleHeading} />}
