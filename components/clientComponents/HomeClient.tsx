36119a1322253abc6f9c2251eb289a1be3b540d0
Mikhail Olianenko <mikhail.olianenko@gmail.com>
2025-03-19 20:16:20 +1100

    новая главная

---
+++ components/clientComponents/HomeClient.tsx
@@ -0,0 +1,8 @@
+'use client'
+import stls from '@/styles/pages/PageHome.module.sass'
+
+const HomeClient = () => {
+  return <div className={stls.container}>Home</div>
+}
+
+export default HomeClient
