8f5cac73a240b815967b64327c8b5340d3d4e214
Mikhail Olianenko <mikhail.olianenko@gmail.com>
2025-03-20 23:41:47 +1100

    ваши дипломы

--- components/clientComponents/HomeClient.tsx
+++ components/clientComponents/HomeClient.tsx
@@ -3,14 +3,50 @@ import stls from '@/styles/pages/PageHome.module.sass'
 import HelpWithChoice from '@/components/sections/Home/HelpWithChoice/HelpWithChoice'
 import AdventuresCards from '../sections/Incomers/AdventuresCards/AdventuresCards'
 import dynamic from 'next/dynamic'
+import StudyProcess, {
+  StudyProcessData
+} from '@/components/sections/Incomers/StudyProcess/StudyProcess'
+import slide1 from '@/public/assets/imgs/home/studyProcess/slide1.png'
+import slide2 from '@/public/assets/imgs/home/studyProcess/slide2.png'
+import slide3 from '@/public/assets/imgs/home/studyProcess/slide3.png'
+import slide4 from '@/public/assets/imgs/home/studyProcess/slide4.png'
+import { homePositionsByBreakpoint } from '@/components/sections/Incomers/StudyProcess/constants'
 
 const PsyTest = dynamic(() => import('@/components/sections/PsyTest'), { ssr: false })
+
+const data: StudyProcessData = {
+  title: 'Ваши будущие дипломы',
+  homePage: true,
+  text: [
+    {
+      bold: 'Все наши программы лицензированы, ',
+      normal:
+        'а дипломы имеют международные приложения, поэтому они ценятся клиентами и профессиональным психологическим сообществом как в России, так и за рубежом!'
+    },
+    {
+      bold: ' получают официальный документ установленного образца, ',
+      normal:
+        'По окончании программ профессиональной переподготовки и курсов повышения квалификации выпускники института',
+      normal2: 'который вносится в реестр ФРДО.',
+      reversed: true
+    }
+  ],
+  positions: homePositionsByBreakpoint,
+  images: [slide1, slide2, slide3, slide4],
+  imageSizes: [
+    { width: 330, height: 490 },
+    { width: 330, height: 230 },
+    { width: 330, height: 230 },
+    { width: 330, height: 490 }
+  ]
+}
 
 const HomeClient = () => {
   return (
     <div className={stls.container}>
       <AdventuresCards showButton />
       <PsyTest fallbackComponent={HelpWithChoice} isRounded />
+      <StudyProcess studyProcess={data} showButton />
     </div>
   )
 }
--- components/sections/Incomers/StudyProcess/constants.ts
+++ components/sections/Incomers/StudyProcess/constants.ts
@@ -1,4 +1,23 @@
-const positionsByBreakpoint = {
+interface Position {
+  top: string
+  left: string
+  rotate: string
+  scale: number
+}
+
+interface BreakpointPositions {
+  start: Position[]
+  final: Position[]
+}
+
+export interface PositionsByBreakpoint {
+  mobile: BreakpointPositions
+  tablet: BreakpointPositions
+  laptop: BreakpointPositions
+  desktop: BreakpointPositions
+}
+
+const positionsByBreakpoint: PositionsByBreakpoint = {
   mobile: {
     start: [
       { top: '0', left: '-13%', rotate: '-15deg', scale: 0.65 },
@@ -57,4 +76,63 @@ const positionsByBreakpoint = {
   }
 }
 
-export default positionsByBreakpoint
+const homePositionsByBreakpoint: PositionsByBreakpoint = {
+  mobile: {
+    start: [
+      { top: '0', left: '-13%', rotate: '-15deg', scale: 0.65 }, // 1
+      { top: '5%', left: '45%', rotate: '5deg', scale: 0.65 }, // 2
+      { top: '45%', left: '-10%', rotate: '-5deg', scale: 0.7 }, // 3
+      { top: '45%', left: '45%', rotate: '5deg', scale: 0.65 } // 4
+    ],
+    final: [
+      { top: '10%', left: '0%', rotate: '0deg', scale: 0.55 }, // 1 (слева)
+      { top: '10%', left: '30%', rotate: '0deg', scale: 0.55 }, // 2 (центр)
+      { top: '55%', left: '30%', rotate: '0deg', scale: 0.55 }, // 3 (центр)
+      { top: '10%', left: '60%', rotate: '0deg', scale: 0.55 } // 4 (справа)
+    ]
+  },
+  tablet: {
+    start: [
+      { top: '5%', left: '5%', rotate: '-10deg', scale: 1.1 }, // 1
+      { top: '5%', left: '55%', rotate: '5deg', scale: 1.1 }, // 2
+      { top: '40%', left: '5%', rotate: '0', scale: 1 }, // 3
+      { top: '40%', left: '55%', rotate: '0', scale: 1 } // 4
+    ],
+    final: [
+      { top: '15%', left: '0%', rotate: '0deg', scale: 1 }, // 1 (слева)
+      { top: '15%', left: '35%', rotate: '0deg', scale: 1 }, // 2 (центр)
+      { top: '60%', left: '35%', rotate: '0deg', scale: 1 }, // 3 (центр)
+      { top: '15%', left: '70%', rotate: '0deg', scale: 1 } // 4 (справа)
+    ]
+  },
+  laptop: {
+    start: [
+      { top: '120px', left: '8%', rotate: '-15deg', scale: 1.4 }, // 1
+      { top: '120px', left: '60%', rotate: '8deg', scale: 1.4 }, // 2
+      { top: '480px', left: '22%', rotate: '0', scale: 1.3 }, // 3
+      { top: '480px', left: '58%', rotate: '0', scale: 1.3 } // 4
+    ],
+    final: [
+      { top: '20%', left: '5%', rotate: '0deg', scale: 1.3 }, // 1 (слева)
+      { top: '20%', left: '40%', rotate: '0deg', scale: 1.3 }, // 2 (центр)
+      { top: '60%', left: '40%', rotate: '0deg', scale: 1.3 }, // 3 (центр)
+      { top: '20%', left: '75%', rotate: '0deg', scale: 1.3 } // 4 (справа)
+    ]
+  },
+  desktop: {
+    start: [
+      { top: '140px', left: '0%', rotate: '-20deg', scale: 1.3 },
+      { top: '170px', left: '37%', rotate: '10deg', scale: 1.1 },
+      { top: '500px', left: '25%', rotate: '0', scale: 1 },
+      { top: '80px', left: '80%', rotate: '10deg', scale: 1.2 }
+    ],
+    final: [
+      { top: '20%', left: '8%', rotate: '0deg', scale: 0.9 },
+      { top: '22%', left: '36%', rotate: '0deg', scale: 0.9 },
+      { top: '47%', left: '36%', rotate: '0deg', scale: 0.9 },
+      { top: '20%', left: '64%', rotate: '0deg', scale: 0.9 }
+    ]
+  }
+}
+
+export { positionsByBreakpoint, homePositionsByBreakpoint }
