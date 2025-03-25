dd9d02214316fa2ea219e4b9abfe2fb8ea8c6246
Ivan Kolpakov <vanjaklp95@gmail.com>
2025-03-25 18:21:42 +0300

    finish with block

--- components/sections/Home/TitleWithCarousel/Title/Title.tsx
+++ components/sections/Home/TitleWithCarousel/Title/Title.tsx
@@ -1,15 +1,19 @@
 import stls from './Title.module.sass'
 import Wrapper from '@/ui/Wrapper'
+import SignUpButtons from './SignUpButtons'
 
 const Title = () => {
-  
   return (
     <section className={stls.container}>
       <Wrapper>
         <div className={stls.left}>
-
           <h1>Московский Институт Психологии</h1>
-          <p>МИП — это команда практикующих экспертов, отзывчивых кураторов и наставников, которые помогут вам воплотить мечту в реальность и обеспечат старт вашей успешной карьеры в сфере психологии</p>
+          <p>
+            МИП — это команда практикующих экспертов, отзывчивых кураторов и наставников, которые
+            помогут вам воплотить мечту в реальность и обеспечат старт вашей успешной карьеры в
+            сфере психологии
+          </p>
+          <SignUpButtons />
         </div>
         
       </Wrapper>
