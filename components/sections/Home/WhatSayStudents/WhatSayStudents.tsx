64df588fc1b4c82695d74b4693eea6e9da6a3811
dv.udanov <dv.udanov@tensor.ru>
2025-04-01 14:35:42 +0300

    Поправил нейминг

--- components/sections/Home/WhatSayStudents/WhatSayStudents.tsx
+++ components/sections/Home/WhatSayStudents/WhatSayStudents.tsx
@@ -8,7 +8,7 @@ import StudentStory from './StudentStory/StudentStory'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation } from 'swiper/modules'
 import ArrowNavigation from '@/ui/ArrowsNavigation/ArrowsNavigation'
-import ImageGallery from './imageGallery/imageGallery'
+import ImageGallery from './ImageGallery/ImageGallery'
 interface WhatSayStudentsProps {
   data: TReview[]
 }
