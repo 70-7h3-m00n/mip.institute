9b68a0221e3528f446e5bb9076556113864ced40
Mikhail Olianenko <mikhail.olianenko@gmail.com>
2025-04-09 21:55:04 +1000

    BuildYourBrandHero

---
+++ components/pages/PageBuildYourBrand.tsx
@@ -0,0 +1,12 @@
+import stls from '@/styles/pages/PageBuildYourBrand.module.sass'
+import Wrapper from '@/ui/Wrapper'
+
+const PageBuildYourBrand = ({}) => {
+  return (
+    <div className={stls.container}>
+      <Wrapper>пумпум</Wrapper>
+    </div>
+  )
+}
+
+export default PageBuildYourBrand
