a9ad2c749100a7713171c87a56e92484405b0dfb
Mikhail Olianenko <mikhail.olianenko@gmail.com>
2025-04-09 23:09:20 +1000

    хиро

--- components/sections/buildYourBrand/Hero/BuildYourBrandHero/BuildYourBrandHero.tsx
+++ components/sections/buildYourBrand/Hero/BuildYourBrandHero/BuildYourBrandHero.tsx
@@ -1,15 +1,28 @@
 import stls from './BuildYourBrandHero.module.sass'
 import PopupTrigger from '@/ui/PopupTrigger'
-import GroupSupervisionHeroInfo from '../BuildYourBrandHeroInfo/BuildYourBrandHeroInfo'
 import { CldImage } from 'next-cloudinary'
+import GroupSupervisionHeroInfo from '@/components/sections/groupSupervision/Hero/GroupSupervisionHeroInfo/GroupSupervisionHeroInfo'
+import heroBrand from 'constants/buildYourBrand/heroBrand'
+import useBetterMediaQuery from '@/hooks/general/UseBetterMediaQuery'
 
 const BuildYourBrandHero = () => {
+  const isMobileAndTabletLayout = useBetterMediaQuery('(max-width: 768px)')
+
   return (
     <section className={stls.container}>
-      <h1 className={stls.title}>Групповая супервизия</h1>
+      <h1 className={stls.title}>
+        Построй
+        <span className={stls.colored}> личный бренд </span>
+        психолога!
+      </h1>
+      <h3 className={stls.subtitle}>Основы продвижения</h3>
       <div className={stls.imageBlock}>
         <CldImage
-          src='group_supervision_hero_3849e0ac52'
+          src={
+            isMobileAndTabletLayout
+              ? 'build_your_brand_hero_mobile_457051b962'
+              : 'build_your_brand_hero_67dcc4e94e'
+          }
           alt='Групповая супервизия'
           className={stls.image}
           style={{ width: '100%', height: 'auto' }}
@@ -17,7 +30,7 @@ const BuildYourBrandHero = () => {
           height='800'
           crop='fit'
         />
-        <GroupSupervisionHeroInfo />
+        <GroupSupervisionHeroInfo data={heroBrand} />
       </div>
       <div className={stls.btn}>
         <PopupTrigger btn='alpha' cta='signUp' />
--- components/sections/groupSupervision/Hero/GroupSupervisionHeroInfo/GroupSupervisionHeroInfo.tsx
+++ components/sections/groupSupervision/Hero/GroupSupervisionHeroInfo/GroupSupervisionHeroInfo.tsx
@@ -1,10 +1,23 @@
 import stls from '@/components/sections/groupSupervision/Hero/GroupSupervisionHeroInfo/GroupSupervisionHeroInfo.module.sass'
 import heroInfo from 'constants/GroupSupervision/heroInfo'
 
-const GroupSupervisionHeroInfo = () => {
+interface Props {
+  data?: (
+    | {
+        title: string
+        text: string
+      }
+    | {
+        title: string
+        text: JSX.Element
+      }
+  )[]
+}
+
+const GroupSupervisionHeroInfo = ({ data = heroInfo }: Props) => {
   return (
     <ul className={stls.info}>
-      {heroInfo.map((el, idx) => (
+      {data.map((el, idx) => (
         <li key={el.title} className={stls.item}>
           <p className={stls.title}>{el.title}</p>
           <p className={stls.description}>{el.text}</p>
