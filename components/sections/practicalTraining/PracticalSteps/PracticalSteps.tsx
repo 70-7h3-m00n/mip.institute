0c0d34e89a27208f4fac0ba7e93d2b84d24d496c
Mikhail Olianenko <mikhail.olianenko@gmail.com>
2025-04-28 21:17:59 +1000

    шаги с анимацией

--- components/sections/practicalTraining/PracticalSteps/PracticalSteps.tsx
+++ components/sections/practicalTraining/PracticalSteps/PracticalSteps.tsx
@@ -1,28 +1,43 @@
 import useBetterMediaQuery from '@/hooks/general/UseBetterMediaQuery'
 import stls from './PracticalSteps.module.sass'
 import { practicalTrainSteps } from 'constants/practicalTrainSteps'
-import { useState } from 'react'
-import SwiperCore from 'swiper'
+import { useEffect, useRef, useState } from 'react'
+import { Swiper as SwiperCore } from 'swiper'
 import 'swiper/css/effect-fade'
-import { EffectFade, Mousewheel } from 'swiper/modules'
+import { EffectFade } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import CubicBlockThreeSide from '@/components/sections/practicalTraining/PracticalSteps/CubicBlockThreeSide/CubicBlockThreeSide'
 import IconPracticalStepNext from '@/components/icons/IconPracticalStepNext'
 import Wrapper from '@/ui/Wrapper'
 import StepBlocks from '../StepBlocks/StepBlocks'
+import { InView } from 'react-intersection-observer'
 
 const PracticalSteps = () => {
   const [currentIndex, setCurrentIndex] = useState(0)
+  const [animateSteps, setAnimateSteps] = useState(false)
   const isMobileAndTabletLayout = useBetterMediaQuery('(max-width: 768px)')
+  const swiperRef = useRef<SwiperCore | null>(null)
 
   if (!isMobileAndTabletLayout) {
-    SwiperCore.use([Mousewheel, EffectFade])
+    SwiperCore.use([EffectFade])
   }
 
   const handleSlideChange = swiper => {
     const newIndex = swiper.activeIndex
     setCurrentIndex(newIndex)
   }
+
+  const handleInView = inView => {
+    if (inView && !animateSteps) {
+      setAnimateSteps(true)
+    }
+  }
+
+  useEffect(() => {
+    if (swiperRef.current && !isMobileAndTabletLayout) {
+      swiperRef.current.slideTo(currentIndex)
+    }
+  }, [currentIndex])
 
   return (
     <section className={stls.container}>
@@ -44,48 +59,58 @@ const PracticalSteps = () => {
               onSlideChange={handleSlideChange}
               style={{ height: '30rem', width: '100%' }}>
               {practicalTrainSteps.map(el => (
-                <SwiperSlide
-                  key={`${el.id}+${el.title}`}
-                  className={stls.slide}>
+                <SwiperSlide key={`${el.id}+${el.title}`} className={stls.slide}>
                   <CubicBlockThreeSide
                     title={el.title}
                     subtitle={el.subtitle}
                     src={el.src}
                     mobHeight={el.mobHeight}
                     fullText={el.fullText}
+                    link={el.link}
                   />
                 </SwiperSlide>
               ))}
             </Swiper>
           )}
-          <Swiper
-            direction={isMobileAndTabletLayout ? 'horizontal' : 'vertical'}
-            slidesPerView={1}
-            spaceBetween={isMobileAndTabletLayout ? 10 : 0}
-            mousewheel={
-              !isMobileAndTabletLayout ? { releaseOnEdges: true } : false
-            }
-            effect={isMobileAndTabletLayout ? 'slide' : 'fade'}
-            fadeEffect={
-              !isMobileAndTabletLayout ? { crossFade: true } : undefined
-            }
-            speed={800}
-            className={stls.mySwiper}
-            onSlideChange={handleSlideChange}
-            style={{ height: '30rem', width: '100%' }}>
-            <StepBlocks currentIndex={currentIndex} />
-            {practicalTrainSteps.map(el => (
-              <SwiperSlide key={`${el.id}+${el.title}`} className={stls.slide}>
-                <CubicBlockThreeSide
-                  title={el.title}
-                  subtitle={el.subtitle}
-                  src={el.src}
-                  mobHeight={el.mobHeight}
-                  fullText={el.fullText}
-                />
-              </SwiperSlide>
-            ))}
-          </Swiper>
+          <InView
+            as='div'
+            onChange={handleInView}
+            triggerOnce
+            threshold={0.7}
+            rootMargin='0px 0px -100px 0px'>
+            <Swiper
+              direction={isMobileAndTabletLayout ? 'horizontal' : 'vertical'}
+              slidesPerView={1}
+              spaceBetween={isMobileAndTabletLayout ? 10 : 0}
+              mousewheel={false}
+              effect={isMobileAndTabletLayout ? 'slide' : 'fade'}
+              fadeEffect={!isMobileAndTabletLayout ? { crossFade: true } : undefined}
+              speed={800}
+              className={stls.mySwiper}
+              onSlideChange={handleSlideChange}
+              onSwiper={swiper => (swiperRef.current = swiper)}
+              style={{ height: '30rem', width: '100%' }}>
+              <StepBlocks
+                currentIndex={currentIndex}
+                setCurrentIndex={setCurrentIndex}
+                animateSteps={animateSteps}
+              />
+              {practicalTrainSteps.map((el, index) => (
+                <SwiperSlide key={`${el.id}+${el.title}`} className={stls.slide}>
+                  <CubicBlockThreeSide
+                    title={el.title}
+                    subtitle={el.subtitle}
+                    src={el.src}
+                    mobHeight={el.mobHeight}
+                    fullText={el.fullText}
+                    isActive={currentIndex === index}
+                    onClick={() => setCurrentIndex(index)}
+                    link={el.link}
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </InView>
         </div>
       </Wrapper>
     </section>
--- components/sections/practicalTraining/StepBlocks/StepBlocks.tsx
+++ components/sections/practicalTraining/StepBlocks/StepBlocks.tsx
@@ -2,20 +2,46 @@ import stls from './StepBlocks.module.sass'
 import classNames from 'classnames'
 import { useEffect, useState } from 'react'
 
-const StepBlocks = ({ currentIndex }) => {
+const StepBlocks = ({ currentIndex, setCurrentIndex, animateSteps }) => {
   const [filledBlocks, setFilledBlocks] = useState([true, false, false])
 
   useEffect(() => {
-    setFilledBlocks(prev => {
-      const newFilledBlocks = [...prev]
-      for (let i = 0; i < newFilledBlocks.length; i++) {
-        newFilledBlocks[i] = i <= currentIndex
+    let intervalId
+
+    if (animateSteps) {
+      const animate = () => {
+        filledBlocks.forEach((_, index) => {
+          setTimeout(() => {
+            setFilledBlocks(prev => {
+              const newFilled = [...prev]
+              newFilled[index] = true
+              return newFilled
+            })
+            setCurrentIndex(index)
+          }, index * 1500)
+        })
       }
-      return newFilledBlocks
-    })
-  }, [currentIndex])
+
+      animate()
+    } else {
+      setFilledBlocks(prev => {
+        const newFilledBlocks = [...prev]
+        for (let i = 0; i < newFilledBlocks.length; i++) {
+          newFilledBlocks[i] = i <= currentIndex
+        }
+        return newFilledBlocks
+      })
+    }
+
+    return () => clearInterval(intervalId)
+  }, [animateSteps, setCurrentIndex])
+
+  const handleStepClick = index => {
+    setCurrentIndex(index)
+  }
 
   const blocks = [{ id: 0 }, { id: 1 }, { id: 2 }]
+
   return (
     <div className={stls.container}>
       {blocks.map(el => (
@@ -23,7 +49,8 @@ const StepBlocks = ({ currentIndex }) => {
           key={el.id}
           className={classNames(stls.stepBlock, {
             [stls.active]: filledBlocks[el.id]
-          })}>
+          })}
+          onClick={() => handleStepClick(el.id)}>
           <span
             className={classNames(stls.number, {
               [stls.completed]: filledBlocks[el.id]
